d4e6e2945ba1e9b1047ceffdba9b3f62e4d6826b
unknown <kasturikh001@gmail.com>
2022-03-08 01:54:06 +0530

    Chrome tester

---
+++ ChromeTester.java
@@ -0,0 +1,31 @@
+class ChromeTester{
+
+public static void main(String[] args){
+	
+	
+	Chrome chr=new Chrome();
+	System.out.println(chr.name + chr.version );
+	chr.chrome();
+	
+	InternetExplorer exp=new InternetExplorer();
+	exp.name = "Web";
+	exp.version = 97.1f;
+	System.out.println(exp.name + exp.version);
+	
+	
+	exp.display();
+	
+	
+	Chrome chrome=new InternetExplorer();
+	
+	
+	
+	
+	
+
+
+
+
+
+}
+}
